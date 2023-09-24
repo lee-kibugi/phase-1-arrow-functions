f746305c46eac6b06894224dfe2a45369d115561
linda wanjiku <lindashykooh@gmail.com>
2023-09-24 10:10:19 +0300

    completed assignments

---
+++ index.js
@@ -0,0 +1,15 @@
+ 
+const divide=(x,y)=>2000/100
+const square = (num) => {
+    return num * num;
+};
+
+const add = (a, b) => {
+    return a + b;
+};
+
+module.exports = {
+    divide,
+    square,
+    add
+};
